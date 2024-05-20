c069dcfb811d9e6c4d703a651ebd59452046d53c
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2024-05-20 16:53:57 +0530

    HomeScreen
    -- getting the products and the displaying it on the Home Screen
    
    -- implementing react router to load the pages faster

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import App from './App';
+import {
+  createBrowserRouter, 
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom'
 import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
+import HomeScreen from "../src/screens/HomeScreen"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />}></Route>
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
