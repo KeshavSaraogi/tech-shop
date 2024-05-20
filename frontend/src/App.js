6290af01e98a3b680b388d43db271b65fea41ebc
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2024-05-20 12:28:10 +0530

    components
    -- Header
    -- Footer
    -- installing bootstrap dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
-    <div>Welcome To Tech Shop</div>
+    <>
+    <Header />
+    <main className="py-3">
+      <Container>
+        <h1>Welcome To Tech Shop</h1>
+      </Container>
+    </main>
+    <Footer />
+    </>
   )
 }
 
