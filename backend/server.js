dd012ebc859a4077dd63cc544a9e3deaa624ba6f
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2024-05-21 12:03:33 +0530

    models
    -- mongoose models for users, orders and products

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import products from "./data/products.js"
-
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+
 dotenv.config()
+connectDB()
 
 const port = process.env.PORT || 5001
 const app = express()
@@ -19,6 +21,5 @@ app.get('/api/products/:id', (req, res) => {
     const product = products.find((p) => p._id === req.params.id)
     res.json(product)
 })
-
 
 app.listen(port, () => console.log(`Server Is Running on port ${port}`))
