1b89cd2bae03d61da598d05721063bd8eecd3ea1
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2024-05-21 17:34:20 +0530

    database
    -- updating routes
    -- creating custom middleware
    -- product controllers

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
-import express from 'express'
-import products from "./data/products.js"
 import dotenv from 'dotenv'
+import express from 'express'
 import connectDB from './config/db.js'
+import products from "./data/products.js"
+import productRoutes from "./routes/productRoutes.js"
+import { notFound, errorHandler } from './middleware/errorMiddleware.js '
 
 dotenv.config()
 connectDB()
@@ -13,6 +15,8 @@ app.get('/', (req, res) => {
     res.send('API IS RUNNING')
 })
 
+app.use('/api/products', productRoutes)
+
 app.get('/api/products', (req, res) => {
     res.json(products)
 })
@@ -21,5 +25,8 @@ app.get('/api/products/:id', (req, res) => {
     const product = products.find((p) => p._id === req.params.id)
     res.json(product)
 })
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server Is Running on port ${port}`))
